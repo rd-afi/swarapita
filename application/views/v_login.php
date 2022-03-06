5323422b4276770957c4c94f89853e27cbb2fdb5
dasdopurba <45941350+dasdopurba18@users.noreply.github.com>
2022-03-06 18:50:21 +0700

    update v_login.php
    add color in v_login, biar cantik gitu tombol backnya..

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -42,8 +42,9 @@ $this->load->view('layout/head');
               </label>
             </div> -->
           </div>
+         
           <button type="submit" value="Login" class="btn btn-primary btn-block">Sign In</button>
-          <a class="btn btn-primary btn-block" href="<?php echo base_url();?>general/index" role="button">Back</a>
+          <a class="btn btn-secondary btn-block" href="<?php echo base_url();?>general/index" role="button" >Back</a>
           <!-- /.col -->
          
           
