04cfd10a95cec88c3bbd2c407b480092d2004c4e
dasdopurba <45941350+dasdopurba18@users.noreply.github.com>
2022-03-31 02:30:39 +0700

    final UAT and add data dummy

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -1,81 +1,75 @@
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
     <div class="container">
-        <a href="<?php echo base_url();?>" class="navbar-brand">
-        <img src="<?php echo base_url();?>assets/dist/img/fococlipping-20220306-173417.png" alt="Gerindra Logo" class="brand-image">
-        <!-- <img src="<?php echo base_url();?>assets/dist/img/Logo_Partai_Gerindra.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
-        <span class="brand-text font-weight-light"><b>SWARAPITA</b></span>
+        <a href="<?php echo base_url(); ?>" class="navbar-brand">
+            <img src="<?php echo base_url(); ?>assets/dist/img/fococlipping-20220306-173417.png" alt="Gerindra Logo" class="brand-image">
+            <!-- <img src="<?php echo base_url(); ?>assets/dist/img/Logo_Partai_Gerindra.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
+            <span class="brand-text font-weight-light"><b>SWARAPITA</b></span>
         </a>
         <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-        
-        <?php if (isset($_SESSION['status']) && $_SESSION['status'] == 'login') { ?>
 
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-            <a href="<?php echo base_url();?>dashboard" class="nav-link">Dashboard</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Menu</a>
-                <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-                    <?php if ($_SESSION['role'] == 'admin') { ?>
-                        <li><a href="<?php echo base_url();?>register_account" class="dropdown-item">Register Account</a></li>
-                        <li><a href="<?php echo base_url();?>admin/list_account" class="dropdown-item">List Account</a></li>
-                    <?php } ?>
-                <li><a href="<?php echo base_url();?>" class="dropdown-item">Form Relawan</a></li>
-                <li><a href="<?php echo base_url();?>list_relawan" class="dropdown-item">List Relawan</a></li>
-                <!-- <li><a href="<?php echo base_url();?>upload_kpu" class="dropdown-item">Upload Data KPU</a></li> -->
-                </ul>
-            </li>
-        </ul>
-        <?php } else { ?>
-            <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <a class="btn-floating btn btn-tw" type="button" role="button" title="Share on facebook"
-             href="https://www.facebook.com/Swara-Pita-108745171600038" target="_blank"
-             rel="noopener">
-             <img src="<?php echo base_url();?>assets/dist/img/24-249482_find-us-on-facebook-transparent-png-stickpng-find-removebg-preview.png" alt="facebook Logo" class="brand-image">
-            </a>
-        </ul>
-        <ul class="navbar-nav">
-            <a class="btn-floating btn btn-tw" type="button" role="button" title="Share on twitter"
-             href="https://twitter.com/SwaraPita" target="_blank"
-             rel="noopener">
-             <img src="<?php echo base_url();?>assets/dist/img/twitter-removebg-preview.png" alt="Twitter Logo" class="brand-image">
-            </a>
-        </ul>
-        <ul class="navbar-nav">
-            <a class="btn-floating btn btn-tw" type="button" role="button" title="Share on instgram"
-             href="https://www.instagram.com/swara.pita/" target="_blank"
-             rel="noopener">
-             <img src="<?php echo base_url();?>assets/dist/img/14844-removebg-preview.png" alt="Gerindra Logo" width="100px" heigth="50px">
-            </a>
-        </ul>
-        <ul class="navbar-nav">
-             <img src="<?php echo base_url();?>assets/dist/img/detail_swarapita-removebg-preview.png" alt="Gerindra Logo" width="480px" heigth="600px">
-            
-        </ul>
-        <?php } ?>
+            <?php if (isset($_SESSION['status']) && $_SESSION['status'] == 'login') { ?>
 
-        <!-- Right navbar links -->
-        <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
-        <?php if (isset($_SESSION['status']) && $_SESSION['status'] == 'login') { ?>
-            <li class="nav-item dropdown">
-                <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Hello, <?php echo (isset($_SESSION['status'])) ? $_SESSION['username'] : NULL ; ?></a>
-                <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-                <li><a href="<?php echo base_url();?>logout" class="dropdown-item">Logout</a></li>
+                <!-- Left navbar links -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="<?php echo base_url(); ?>dashboard" class="nav-link">Dashboard</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Menu</a>
+                        <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                            <?php if ($_SESSION['role'] == 'super_admin') { ?>
+                                <li><a href="<?php echo base_url(); ?>register_account" class="dropdown-item">Register Account</a></li>
+                                <li><a href="<?php echo base_url(); ?>admin/list_account" class="dropdown-item">List Account</a></li>
+                            <?php } ?>
+                            <li><a href="<?php echo base_url(); ?>" class="dropdown-item">Form Relawan</a></li>
+                            <li><a href="<?php echo base_url(); ?>list_relawan" class="dropdown-item">List Relawan</a></li>
+                            <!-- <li><a href="<?php echo base_url(); ?>upload_kpu" class="dropdown-item">Upload Data KPU</a></li> -->
+                        </ul>
+                    </li>
                 </ul>
-            </li>
             <?php } else { ?>
-                <li class="nav-item">
-                    <a href="<?php echo base_url();?>login" class="nav-link">Login</a>
-                </li>
-            <?php } ?>
-        </ul>
+                <!-- Left navbar links -->
+                <ul class="navbar-nav">
+                    <a class="btn-floating btn btn-tw" type="button" role="button" title="Share on facebook" href="https://www.facebook.com/Swara-Pita-108745171600038" target="_blank" rel="noopener">
+                        <img src="<?php echo base_url(); ?>assets/dist/img/24-249482_find-us-on-facebook-transparent-png-stickpng-find-removebg-preview.png" alt="facebook Logo" class="brand-image">
+                    </a>
+                </ul>
+                <ul class="navbar-nav">
+                    <a class="btn-floating btn btn-tw" type="button" role="button" title="Share on twitter" href="https://twitter.com/SwaraPita" target="_blank" rel="noopener">
+                        <img src="<?php echo base_url(); ?>assets/dist/img/twitter-removebg-preview.png" alt="Twitter Logo" class="brand-image">
+                    </a>
+                </ul>
+                <ul class="navbar-nav">
+                    <a class="btn-floating btn btn-tw" type="button" role="button" title="Share on instgram" href="https://www.instagram.com/swara.pita/" target="_blank" rel="noopener">
+                        <img src="<?php echo base_url(); ?>assets/dist/img/14844-removebg-preview.png" alt="Gerindra Logo" width="100px" heigth="50px">
+                    </a>
+                </ul>
+                <ul class="navbar-nav">
+                    <img src="<?php echo base_url(); ?>assets/dist/img/detail_swarapita-removebg-preview.png" alt="Gerindra Logo" width="480px" heigth="600px">
 
-    </div>
+                </ul>
+            <?php } ?>
+
+            <!-- Right navbar links -->
+            <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
+                <?php if (isset($_SESSION['status']) && $_SESSION['status'] == 'login') { ?>
+                    <li class="nav-item dropdown">
+                        <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Hello, <?php echo (isset($_SESSION['status'])) ? $_SESSION['username'] : NULL; ?></a>
+                        <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                            <li><a href="<?php echo base_url(); ?>logout" class="dropdown-item">Logout</a></li>
+                        </ul>
+                    </li>
+                <?php } else { ?>
+                    <li class="nav-item">
+                        <a href="<?php echo base_url(); ?>login" class="nav-link">Login</a>
+                    </li>
+                <?php } ?>
+            </ul>
+
+        </div>
 </nav>
 <!-- /.navbar -->
--- application/views/list_relawan.php
+++ application/views/list_relawan.php
@@ -10,118 +10,135 @@ $this->load->view('layout/head');
 ?>
 
 <body class="hold-transition layout-top-nav">
-<div class="wrapper">
+  <div class="wrapper">
 
-<?php
-$this->load->view('layout/navbar');
-?>
+    <?php
+    $this->load->view('layout/navbar');
+    ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">List Relawan Swarapita</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="<?php echo base_url();?>">Home</a></li>
-              <li class="breadcrumb-item"><a href="#">List Relawan</a></li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">List Relawan Swarapita</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">List Relawan</a></li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-12 col-6">
-          <div class="card">
-              <!-- <div class="card-header">
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <!-- Small boxes (Stat box) -->
+          <div class="row">
+            <div class="col-lg-12 col-6">
+              <div class="card">
+                <!-- <div class="card-header">
                 <h3 class="card-title">List Relawan</h3>
               </div> -->
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table id="list1" class="table display table-bordered table-striped nowrap" data-export-title="List Relawan SWARAPITA">
-                  <thead>
-                  <tr>
-                    <th>NIK</th>
-                    <th>Nama</th>
-                    <th>Jenis Kelamin</th>
-                    <th>Tempat Lahir</th>
-                    <th>Tanggal Lahir</th>
-                    <th>Alamat</th>
-                    <th>Kota</th>
-                    <th>Kecamatan</th>
-                    <th>Kelurahan</th>
-                    <th>No HP / WA</th>
-                    <th>Relawan</th>
-                    <th>Lokasi</th>
-                    <th>Input By</th>
-                    <th>Action</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <?php 
-                    $no = 1;
-                    foreach($relawan as $u){ 
-                    ?>
-                    <tr>
-                        <td><?php echo $u->nik ?></td>
-                        <td><?php echo $u->nama ?></td>
-                        <td><?php echo ($u->jk == 1) ? 'Laki-Laki' : 'Perempuan'; ?></td>
-                        <td><?php echo $u->tempat_lahir?></td>
-                        <td><?php echo $u->tgl_lahir?></td>
-                        <td><?php echo $u->alamat ?></td>
-                        <td><?php echo $u->kabupaten ?></td>
-                        <td><?php echo $u->kecamatan ?></td>
-                        <td><?php echo $u->kelurahan ?></td>
-                        <td><?php echo $u->hpwa ?></td>
-                        <td><?php echo ($u->as_koor == 1) ? 'Ya Bersedia' : 'Tidak Bersedia'; ?></td>
-                        <td><?php echo $u->lokasi ?></td>
-                        <td><?php echo $u->penginput ?></td>
-                        <?php 
-                        $role=$u->penginput;
-                        $session=$_SESSION['username'];
-                        if ($session=='admin'){ ?>
-                          <td><a type="button" href="<?php echo base_url('edit/'.$u->nik);?>" class="btn btn-warning btn-sm"><i class="fas fa-pen"></i></a>
-                              <a type="button" href="<?php echo base_url('hapus/'.$u->nik);?>" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
-                          </td>
-                        <?php } else if($role==$session){ ?>
-                          <td><a type="button" href="<?php echo base_url('edit/'.$u->nik);?>" class="btn btn-warning btn-sm"><i class="fas fa-pen"></i></a>
-                              <a type="button" href="<?php echo base_url('hapus/'.$u->nik);?>" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
-                          </td>
-                        <?php } else { ?>
-                          <td></td>
-                        <?php } ?>
-                    </tr>
-                  <?php } ?>
-                  </tbody>
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <table id="list1" class="table display table-bordered table-striped nowrap" data-export-title="List Relawan SWARAPITA">
+                    <thead>
+                      <tr>
+                        <th>NIK</th>
+                        <th>Nama</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Tempat Lahir</th>
+                        <th>Tanggal Lahir</th>
+                        <th>Alamat</th>
+                        <th>Kota</th>
+                        <th>Kecamatan</th>
+                        <th>Kelurahan</th>
+                        <th>No HP / WA</th>
+                        <th>Relawan</th>
+                        <th>Lokasi</th>
+                        <?php
+                        $session = $_SESSION['role'];
+                        if ($session == 'super_admin') {
+                        ?>
+                          <th>Input By</th>
+                        <?php } else {
+                        } ?>
+                        <?php
+                        $session = $_SESSION['role'];
+                        if ($session == 'super_admin' or $session == 'admin') {
+                        ?>
+                          <th>Action</th>
+                        <?php } else {
+                        } ?>
 
-                </table>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php
+                      $no = 1;
+                      foreach ($relawan as $u) {
+                      ?>
+                        <tr>
+                          <td><?php echo $u->nik ?></td>
+                          <td><?php echo $u->nama ?></td>
+                          <td><?php echo ($u->jk == 1) ? 'Laki-Laki' : 'Perempuan'; ?></td>
+                          <td><?php echo $u->tempat_lahir ?></td>
+                          <td><?php echo $u->tgl_lahir ?></td>
+                          <td><?php echo $u->alamat ?></td>
+                          <td><?php echo $u->kabupaten ?></td>
+                          <td><?php echo $u->kecamatan ?></td>
+                          <td><?php echo $u->kelurahan ?></td>
+                          <td><?php echo $u->hpwa ?></td>
+                          <td><?php echo ($u->as_koor == 1) ? 'Ya' : 'Tidak'; ?></td>
+                          <td><?php echo $u->lokasi ?></td>
+                          <?php
+                          $session = $_SESSION['role'];
+                          if ($session == 'super_admin') {
+                          ?>
+                            <td><?php echo $u->penginput ?></td>
+                          <?php } else {
+                          } ?>
+
+                          <?php
+                          $role = $u->penginput;
+                          $session = $_SESSION['role'];
+                          if ($session == 'super_admin' or $session == 'admin') { ?>
+                            <td><a type="button" href="<?php echo base_url('edit/' . $u->nik); ?>" class="btn btn-warning btn-sm"><i class="fas fa-pen"></i></a>
+                              <a type="button" href="<?php echo base_url('hapus/' . $u->nik); ?>" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
+                            </td>
+                          <?php } else {  ?>
+
+
+                          <?php } ?>
+                        </tr>
+                      <?php } ?>
+                    </tbody>
+
+                  </table>
+                </div>
+                <!-- /.card-body -->
               </div>
-              <!-- /.card-body -->
+              <!-- /.card -->
             </div>
-            <!-- /.card -->
           </div>
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
 
-  </div>
-  <!-- /.content-wrapper -->
+    </div>
+    <!-- /.content-wrapper -->
 
     <!-- Control Sidebar -->
     <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
+      <!-- Control sidebar content goes here -->
     </aside>
     <!-- /.control-sidebar -->
 
@@ -129,94 +146,119 @@ $this->load->view('layout/navbar');
     <footer class="main-footer">
 
     </footer>
-</div>
-<!-- ./wrapper -->
+  </div>
+  <!-- ./wrapper -->
 
-<!-- REQUIRED SCRIPTS -->
-<?php
-$this->load->view('layout/js');
-?>
+  <!-- REQUIRED SCRIPTS -->
+  <?php
+  $this->load->view('layout/js');
+  ?>
 
-<script>
-
-  $(function () {
-    $("#list1").DataTable({
-      "autoWidth": false, "scrollX": true,
-      "buttons": [
-        {
+  <script>
+    $(function() {
+      $("#list1").DataTable({
+        "autoWidth": false,
+        "scrollX": true,
+        "buttons": [{
           extend: 'excelHtml5',
           title: "List Relawan SWARAPITA",
           filename: "List Relawan SWARAPITA",
           exportOptions: {
-            columns: [0,1,2,3,4,5,6,7,8,9,10,11]
+            columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
             // columns: ':visible:not(:last-child)'
           }
-        },{
+        }, {
           extend: 'csvHtml5',
           title: "List Relawan SWARAPITA",
           filename: "List Relawan SWARAPITA",
           exportOptions: {
-            columns: [0,1,2,3,4,5,6,7,8,9,10,11]
+            columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
             // columns: ':visible:not(:last-child)'
           }
-        },{
+        }, {
           extend: 'pdfHtml5',
           title: "List Relawan SWARAPITA",
           filename: "List Relawan SWARAPITA",
+          orientation: 'landscape',
+          pageSize: 'LEGAL',
           exportOptions: {
-            columns: [0,1,2,3,4,5,6,7,8,9,10,11]
+            columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
             // columns: ':visible:not(:last-child)'
           }
-        }
-      ],
-      "fixedColumns": { right: 1 },
-      initComplete: function () {
-        $('<div class="row">' +
-          '<div class="col-sm-4"> <div class="form-group">' +
-            '<select class="form-control" id="fKot"><option value="">KOTA</option></select>' +
-          '</div> </div>' +
-          '<div class="col-sm-4"> <div class="form-group">' +
+        }],
+        "fixedColumns": {
+          right: 1
+        },
+        initComplete: function() {
+          $('<div class="row">' +
+            '<div class="col-sm-4"> <div class="form-group">' +
+            '<select class="form-control" id="fKot" ><option value="">KOTA</option></select>' +
+            '</div> </div>' +
+            '<div class="col-sm-4"> <div class="form-group">' +
             '<select class="form-control" id="fKec"><option value="">KECAMATAN</option></select>' +
-          '</div> </div>' +
-          '<div class="col-sm-4"> <div class="form-group">' +
+            '</div> </div>' +
+            '<div class="col-sm-4"> <div class="form-group">' +
             '<select class="form-control" id="fKel"><option value="">KELURAHAN</option></select>' +
-          '</div> </div>' +
-        '</div>').appendTo("#list1_wrapper .dataTables_filter");
-            this.api().columns(6).every( function () {
-                var column = this;
-                var select = $("#fKot")
-                    .on( 'change', function () { var val = $.fn.dataTable.util.escapeRegex( $(this).val());
-                        column.search( val ? '^'+val+'$' : '', true, false ).draw();
-                    } );
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' )
-                } );
-            } );
-            this.api().columns(7).every( function () {
-                var column = this;
-                var select = $("#fKec")
-                    .on( 'change', function () { var val = $.fn.dataTable.util.escapeRegex( $(this).val());
-                        column.search( val ? '^'+val+'$' : '', true, false ).draw();
-                    } );
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' )
-                } );
-            } );
-            this.api().columns(8).every( function () {
-                var column = this;
-                var select = $("#fKel")
-                    .on( 'change', function () { var val = $.fn.dataTable.util.escapeRegex( $(this).val());
-                        column.search( val ? '^'+val+'$' : '', true, false ).draw();
-                    } );
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' )
-                } );
-            } );
-            
+            '</div> </div>' +
+            '</div>').appendTo("#list1_wrapper .dataTables_filter");
+          this.api().columns(6).every(function() {
+            var column = this;
+            var select = $("#fKot")
+              .on('change', function() {
+                if ($(this).val() == "") {
+                  document.getElementById("fKec").removeAttribute("hidden");
+                  document.getElementById("fKel").removeAttribute("hidden");
+                } else {
+                  document.getElementById("fKec").setAttribute("hidden", "true");
+                  document.getElementById("fKel").setAttribute("hidden", "true");
+                }
+                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                column.search(val ? '^' + val + '$' : '', true, false).draw();
+              });
+            column.data().unique().sort().each(function(d, j) {
+              select.append('<option value="' + d + '">' + d + '</option>')
+            });
+          });
+          this.api().columns(7).every(function() {
+            var column = this;
+            var select = $("#fKec")
+              .on('change', function() {
+                if ($(this).val() == "") {
+                  document.getElementById("fKot").removeAttribute("hidden");
+                  document.getElementById("fKel").removeAttribute("hidden");
+                } else {
+                  document.getElementById("fKot").setAttribute("hidden", "true");
+                  document.getElementById("fKel").setAttribute("hidden", "true");
+                }
+                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                column.search(val ? '^' + val + '$' : '', true, false).draw();
+              });
+            column.data().unique().sort().each(function(d, j) {
+              select.append('<option value="' + d + '">' + d + '</option>')
+            });
+          });
+          this.api().columns(8).every(function() {
+            var column = this;
+            var select = $("#fKel")
+              .on('change', function() {
+                if ($(this).val() == "") {
+                  document.getElementById("fKot").removeAttribute("hidden");
+                  document.getElementById("fKec").removeAttribute("hidden");
+                } else {
+                  document.getElementById("fKot").setAttribute("hidden", "true");
+                  document.getElementById("fKec").setAttribute("hidden", "true");
+                }
+                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                column.search(val ? '^' + val + '$' : '', true, false).draw();
+              });
+            column.data().unique().sort().each(function(d, j) {
+              select.append('<option value="' + d + '">' + d + '</option>')
+            });
+          });
         }
-    }).buttons().container().appendTo('#list1_wrapper .col-md-6:eq(0)');
-  });
-
-</script>
+      }).buttons().container().appendTo('#list1_wrapper .col-md-6:eq(0)');
+    });
+  </script>
 </body>
+
 </html>
